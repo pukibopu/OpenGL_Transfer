f54351bee0a3e825a6f40e94c36f5fee7605b1f3
jinjo <creationer625@outlook.com>
2022-10-28 14:21:54 +0800

    Initial Commit

---
+++ Transfer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Transfer
+//
+//  Created by ShuYang Yan on 2022/10/28.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
